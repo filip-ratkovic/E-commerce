61c418ac1b98d65d0c9ae13da4cce2018f5654a1
Filip <filip.ratkovic.fr@gmail.com>
2022-09-02 23:21:47 +0200

    NavBar podesavanje

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import {Box} from '@mui/material'
+import {NavBar, Feed, SearchFeed, VideoDetail, ChannelDetail} from './components'
 
-function App() {
-  return (
-    <div>App</div>
-  )
-}
+
+const App = () => (
+<BrowserRouter>
+<Box >
+  <NavBar/>
+
+  <Routes>
+    <Route path="/" element={<Feed />} />
+    <Route path="/video/:id" element={<VideoDetail/>} />
+    <Route path="/channel/:id" element={<ChannelDetail/>} />
+    <Route path="/" element={<Feed />} />
+    <Route path="/search/:searchTerm" element={<SearchFeed/>} />
+  </Routes>
+
+</Box>
+</BrowserRouter>
+) 
 
 export default App
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import {useState} from 'react'
+import {Navigation} from 'react-router-dom'
+import {Paper, IconButter} from '@mui/material'
+import {SearchIcon} from '@mui/icons-material'
+function SearchBar() {
+  return (
+    <div>SearchBar</div>
+  )
+}
+
+export default SearchBar
