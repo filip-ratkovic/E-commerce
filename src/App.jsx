acbdb1c198486d02fe362c512aeef58b04565944
Filip <filip.ratkovic.fr@gmail.com>
2022-10-07 23:45:26 +0200

    Change

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,7 @@
 import React from 'react'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import {Box} from '@mui/material'
-import {NavBar, Feed, SearchFeed, VideoDetail, ChannelDetail} from './components'
-
-
 const App = () => (
 <BrowserRouter>
-<Box >
-  <NavBar/>
 
-  <Routes>
-    <Route path="/" element={<Feed />} />
-    <Route path="/video/:id" element={<VideoDetail/>} />
-    <Route path="/channel/:id" element={<ChannelDetail/>} />
-    <Route path="/" element={<Feed />} />
-    <Route path="/search/:searchTerm" element={<SearchFeed/>} />
-  </Routes>
-
-</Box>
 </BrowserRouter>
 ) 
 
