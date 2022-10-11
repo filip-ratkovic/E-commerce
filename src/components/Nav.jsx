712dcd3a567718214bc0011c62b11293dc3888ec
Filip <filip.ratkovic.fr@gmail.com>
2022-10-11 23:12:44 +0200

    Nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 function Nav() {
   return (
-    <div>
-<Link to="/">Home</Link>
-<Link to="/products">Products</Link>
-<Link to="/contact">Contact</Link>
+    <nav>
+      <div className="nav-logo">
+        <img src="https://react-course-comfy-sloth-store.netlify.app/static/media/logo.221f6b13.svg" alt="logo" style={{ width: "150px" }} />
+      </div>
 
-    </div>
+      <div className="nav-name">
+        <Link to="/">Home</Link>
+        <Link to="/products">Products</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+
+      <div className="nav-cartLogin">
+      <div className='nav-cart'><Link  to="/korpa"> Cart</Link></div>
+      <div className='nav-login'><Link to="/login"> Login</Link></div>
+
+      </div>
+
+    </nav>
   )
 }
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 
+
+
 function Products() {
   return (
     <div>Products</div>
