65bb8bd5de17d59779272ac19112e4132095c9c9
Filip <filip.ratkovic.fr@gmail.com>
2022-10-20 23:00:59 +0200

    Login

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -4,7 +4,25 @@ function Login() {
   return (
     <div className="login-body">
       <div className="login-dropdown" >
-        <h1>nesto</h1>
+        <main className="login-main">
+          <div className="signup-conteiner">
+          <h2 id="signup-title">Sign up</h2>
+            <form action="">
+              <input type="text" name="name" id="signName" placeholder="User name" />
+              <input type="email" name="email" id="signEmail" placeholder="Email" />
+              <input type="password" name="password" id="signPassword" placeholder="Password" /><br />
+              <button className="btn-signup">Sign up</button>
+            </form>
+          </div>
+          <div className="login-conteiner">
+            <h2 id="login-title">Login</h2>
+            <form action="">
+              <input type="email" name="email" id="loginEmail" placeholder="Email" />
+              <input type="password" name="password" id="loginPassword" placeholder="Password" /><br />
+              <button className="btn-signup">Sign up</button>
+            </form>
+          </div>
+        </main>
       </div>
     </div>
   )
