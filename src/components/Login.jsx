362b2730226b047907881ea9bf87d7b5c5618f63
Filip <filip.ratkovic.fr@gmail.com>
2022-10-22 22:49:14 +0200

    Login end

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,31 +6,35 @@ const [loginUp, setLoginUp] = useState(false);
 
 const loginUpHandler = () => {
   if(loginUp) {
+    setLoginUp(curent => !curent);
   }else {
     setLoginUp(curent => !curent);
   }
 }
 
+useEffect(() => {
+
+},[])
 
   return (
     <div className="login-body">
       <div className="login-dropdown" >
         <main className="login-main">
           <div className="signup-conteiner">
-          <h2 id="signup-title">Sign up</h2>
+          <h2 id="signup-title" onClick={loginUpHandler} >Sign up</h2>
             <form action="">
-              <input type="text" name="name" id="signName" placeholder="User name" />
-              <input type="email" name="email" id="signEmail" placeholder="Email" />
-              <input type="password" name="password" id="signPassword" placeholder="Password" /><br />
+              <input type="text" name="name" id="signName" placeholder="User name" required/>
+              <input type="email" name="email" id="signEmail" placeholder="Email" required />
+              <input type="password" name="password" id="signPassword" placeholder="Password"  required/><br />
               <button className="btn-signup">Sign up</button>
             </form>
           </div>
-          <div className= {`${loginUp ? "login-conteiner loginUp-active" : ""}`} onClick={loginUpHandler}>
-            <h2 id="login-title">Login</h2>
+          <div className=  {`login-conteiner ${loginUp ? "loginUp-active" : ""}`}>
+            <h2 id="login-title" onClick={loginUpHandler}>Login</h2>
             <form action="">
-              <input type="email" name="email" id="loginEmail" placeholder="Email" />
-              <input type="password" name="password" id="loginPassword" placeholder="Password" /><br />
-              <button className="btn-signup">Sign up</button>
+              <input type="email" name="email" id="loginEmail" placeholder="Email" required />
+              <input type="password" name="password" id="loginPassword" placeholder="Password" required /><br />
+              <button className="btn-signup">Login</button>
             </form>
           </div>
         </main>
