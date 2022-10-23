0c3bbad42be3a664cf59162eee734ec62a43bf83
Filip <filip.ratkovic.fr@gmail.com>
2022-10-23 22:19:28 +0200

    Loading

---
+++ src/components/Loading.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className="loading-conteiner">
+      <div className="loading-cyrcle">
+        loading
+    </div>
+    </div>
+  )
+}
+
+export default Loading
