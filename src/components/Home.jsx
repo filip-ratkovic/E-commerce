79f1c4b8d09154654e6176b9e7ab71f02faf0d7b
Filip <filip.ratkovic.fr@gmail.com>
2022-11-01 16:59:53 +0100

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,10 +2,23 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>
-      
-    </div>
-  )
+
+      <div>Home
+        <h1> Desing Your <br>
+        </br> Comfort Zone</h1>
+        <p> Lorem ipsum, dolor sit amet consectetur adipisicing elit. <br>
+        </br>
+          Lusto, at sad omnis corporis doloremque possimus velit! <br>
+          </br>Repudiandae nisi odit, aperiam odio ducimus, obcaecati <br>
+          </br>libero et qiia tempora excepturi quis alias?</p>
+
+        <button> SHOP NOW </button>
+        <div>
+          <h1> Featured Products </h1>
+          <button> ALL PRODUCTS </button>
+        </div>
+      </div>
+      )
 }
 
-export default Home
+      export default Home
