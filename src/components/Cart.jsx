9ab277c7bf1f3d48321a4700c8a5905de367b6e7
Filip <filip.ratkovic.fr@gmail.com>
2022-11-02 23:51:37 +0100

    Product page filters

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 
 function Cart({cartData, quantity}) {
+console.log(cartData)
   return (
     <div>
       <h1>{cartData && cartData.name}</h1>
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,14 +7,14 @@ function Products({ getProductsData }) {
   const [productsData, setProductData] = useState();
   const [loading, setLoading] = useState(true);
   const [priceValue, setPriceValue] = useState(3100);
+  const [companyValue, setCompanyValue] = useState("all");
+  const [categoryValue, setCategoryValue] = useState("all");
+  const [shipping, setShipping] = useState(false);
+
 
   const handlePriceValue = (data) => {
     setPriceValue(data);
   }
-const sortZA = () => {
-productsData = productsData.reverse();
-}
-
 
 
   const fetchProductsData = async () => {
@@ -49,50 +49,60 @@ productsData = productsData.reverse();
 
 
       <div className="aside">
-      <input type="search" name="search" id="search" placeholder="Search" />
-      <div className="category">
-        <h3>Category</h3>
-        <ul>
-          <li className="active-list">All</li>
-          <li>Office</li>
-          <li>Living room</li>
-          <li>Kitchen</li>
-          <li>Badroom</li>
-          <li>Dining</li>
-          <li>Kids</li>
-        </ul>
-      </div>
-      <div className="company">
-        <h3>Company</h3>
-        <select name="company" onChange={(el) => {
-            console.log(el.target.value);
-          } }>
-          <option value="all">All</option>
-          <option  value="marcos">Marcos</option>
-          <option value="ikea">Ikea</option>
-          <option value="liddy">Liddy</option>
-        </select>
-      </div>
-      <div className="price">
-        <h3>Price</h3>
-        <form action="price">
-          <p>${priceValue} </p>
-          <input type="range" name="price" id="price" max="3100" min="0" value={priceValue}
-            onChange={(el) => {
-              setPriceValue(el.target.value)
+        <input type="search" name="search" id="search" placeholder="Search" />
+        <div className="category">
+          <h3>Category</h3>
+          <ul onClick={(el) => {
+            setCategoryValue(el.target.innerText)
+          }}>
+            <li>All</li>
+            <li >Office</li>
+            <li>Living room</li>
+            <li>Kitchen</li>
+            <li>Badroom</li>
+            <li>Dining</li>
+            <li>Kids</li>
+          </ul>
+        </div>
+        <div className="company">
+          <h3>Company</h3>
+          <select name="company" onChange={(el) => {
+            setCompanyValue(el.target.value)
+          }}>
+            <option value="all">All</option>
+            <option value="marcos">Marcos</option>
+            <option value="ikea">Ikea</option>
+            <option value="liddy">Liddy</option>
+          </select>
+        </div>
+        <div className="price">
+          <h3>Price</h3>
+          <form action="price">
+            <p>${priceValue} </p>
+            <input type="range" name="price" id="price" max="3100" min="0" value={priceValue}
+              onChange={(el) => {
+                setPriceValue(el.target.value)
+              }} />
+          </form>
+        </div>
+        <div className="free-shipping">
+          <label htmlFor="free">Free shipping</label>
+          <input type="checkbox" name="freeShipping"
+              onChange={(el) => {
+                setShipping(el.target.checked)
             }} />
-        </form>
+        </div>
+        <div className="clear-filters">
+          <button onClick={() => {
+            setPriceValue(3100)
+            setCategoryValue("all")
+            setCompanyValue("all")
+            setShipping(false)
+          }}>Clear filters</button>
+        </div>
       </div>
-      <div className="free-shipping">
-        <label htmlFor="free">Free shipping</label>
-        <input type="checkbox" name="freeShipping" />
-      </div>
-      <div className="clear-filters">
-        <button>Clear filters</button>
-      </div>
-    </div>
 
-  {/* aside aside L */}
+      {/* aside aside L */}
 
 
 
@@ -100,33 +110,38 @@ productsData = productsData.reverse();
       <div className="product-container">
         <div className="product-top">
           <div className="product-left">
-          <div className="product-icons">
-          <img src="https://img.icons8.com/ios/50/228BE6/squared-menu.png"/>
-          <img src="https://img.icons8.com/fluency/48/000000/menu-rounded.png"/>
-          </div>
-          <div className="products-found">
-            <p>{productsData.length} products found</p>
-          </div>
+            <div className="product-icons">
+              <img src="https://img.icons8.com/ios/50/228BE6/squared-menu.png" />
+              <img src="https://img.icons8.com/fluency/48/000000/menu-rounded.png" />
+            </div>
+            <div className="products-found">
+              <p>{productsData.length} products found</p>
+            </div>
           </div>
           <div className="product-sort-by">
             <label htmlFor="">Sort by:</label>
             <select name="sort" id="sort">
-            <option value="price-lowest">Price lowest</option>
-            <option value="price-highest">Price highest</option>
-            <option value="name-a">Name (a - z) </option>
-            <option value="name-z">Name (z - a)</option>
+              <option value="price-lowest">Price lowest</option>
+              <option value="price-highest">Price highest</option>
+              <option value="name-a">Name (a - z) </option>
+              <option value="name-z"
+              >Name (z - a)</option>
             </select>
           </div>
         </div>
-      <div className='product-content'>
-        {
-          productsData.map((product) => {
-            if(product.price < priceValue*100 && product.company === "ikea" ) {
-              return <ProductCard key={product.id} {...product} />
-            }
-          })
-        }
-      </div>
+        <div className='product-content'>
+          {
+            productsData.map((product) => {
+              if (product.price < priceValue * 100 &&
+                (product.company === companyValue || companyValue === "all") &&
+                (product.category === categoryValue.toLocaleLowerCase() || categoryValue.toLocaleLowerCase() === "all") &&
+                (product.shipping === true || shipping === false)
+              ) {
+                return <ProductCard key={product.id} {...product} />
+              }
+            })
+          }
+        </div>
       </div>
     </div>
   )
