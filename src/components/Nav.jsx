f78051747ad5b95eba1f5306c117d6df02fc8d6e
Filip <filip.ratkovic.fr@gmail.com>
2022-11-17 23:52:02 +0100

    Nav bar responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 function Nav() {
@@ -7,7 +7,8 @@ function Nav() {
   const [productsActive, setProductsActive] = useState(false);
   const [cartActive, setCartActive] = useState(false);
   const [loginActive, setLoginActive] = useState(false);
-
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [toggleMenu, setToggleMenu] = useState(false)
 
   const handleHomeClick = event => {
     if (homeActive) {
@@ -49,37 +50,59 @@ function Nav() {
     }
   }
 
+  const handleToggle = () => {
+      setToggleMenu(!toggleMenu)
+  }
+
+  useEffect(() => {
+
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    }
+
+    window.addEventListener('resize', changeWidth)
+    return () => {
+      window.removeEventListener('resize', changeWidth)
+  }
+  }, [])
+
 
 
 
   return (
     <nav>
-      <div className="nav-logo">
-        <img src="https://react-course-comfy-sloth-store.netlify.app/static/media/logo.221f6b13.svg" alt="logo" style={{ width: "150px" }} />
-      </div>
-
-      <div className="nav-name">
-        <Link to="/" >
-          <p className={`${homeActive ? "nav-active" : ""}`} onClick={handleHomeClick}>Home</p>
-        </Link>
-        <Link to="/products" >
-          <p className={`${productsActive ? "nav-active" : ""}`} onClick={handleProductsClick}>Products</p>
-        </Link>
-      </div>
-
-      <div className="nav-cartLogin">
-        <div className='nav-cart'><Link to="/korpa">
-          <span className={`${cartActive ? "nav-active" : ""}`} onClick={handleCartClick}>Cart</span>
-          <img src="https://img.icons8.com/material-rounded/24/000000/shopping-cart.png" alt="cart" />
-          <div className="nav-cart-num">0</div>
-        </Link></div>
-        <div className='nav-login'><Link to="/login">
-          <span className={`${loginActive ? "nav-active" : ""}`} onClick={handleLoginClick}>Login</span>
-          <img src="https://img.icons8.com/ios-glyphs/30/000000/user--v1.png" alt="login" />
-        </Link></div>
-
-      </div>
-
+       <div className="nav-logo">
+          <img src="https://react-course-comfy-sloth-store.netlify.app/static/media/logo.221f6b13.svg" alt="logo" style={{ width: "150px" }} />
+        </div>
+    {(toggleMenu || screenWidth > 550) && (
+       <div className="nav-cont">
+      
+  
+        <div className="nav-name">
+          <Link to="/" >
+            <p className={`${homeActive ? "nav-active" : ""}`} onClick={handleHomeClick}>Home</p>
+          </Link>
+          <Link to="/products" >
+            <p className={`${productsActive ? "nav-active" : ""}`} onClick={handleProductsClick}>Products</p>
+          </Link>
+        </div>
+  
+        <div className="nav-cartLogin">
+          <div className='nav-cart'><Link to="/korpa">
+            <span className={`${cartActive ? "nav-active" : ""}`} onClick={handleCartClick}>Cart</span>
+            <img src="https://img.icons8.com/material-rounded/24/000000/shopping-cart.png" alt="cart" />
+            <div className="nav-cart-num">0</div>
+          </Link></div>
+          <div className='nav-login'><Link to="/login">
+            <span className={`${loginActive ? "nav-active" : ""}`} onClick={handleLoginClick}>Login</span>
+            <img src="https://img.icons8.com/ios-glyphs/30/000000/user--v1.png" alt="login" />
+          </Link></div>
+        </div>
+  
+       </div>
+    )}
+     <button className="nav-btn" onClick={handleToggle}>
+      <img style={{ width:"30px"}} src="https://img.icons8.com/ios-filled/50/null/menu-rounded.png"/></button>
     </nav>
   )
 }
