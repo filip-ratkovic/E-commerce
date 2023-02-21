da344b223d20b645c972c02c9ebb0f8b5f5cd0ec
Filip <filip.ratkovic.fr@gmail.com>
2023-02-21 23:18:40 +0100

    CHange btn

--- src/components/CartItems.jsx
+++ src/components/CartItems.jsx
@@ -37,11 +37,7 @@ function CartItems(props) {
             <section className="subtotal">
               <p>{counter * product.price / 100}</p>
             </section>
-            {/* <button className="delete-btn" onClick={()=>{
-              removeCart(product.id)
-            }}>
-              <img src="https://img.icons8.com/ios-glyphs/25/000000/delete-forever.png" />
-            </button> */}
+           
           </div>
   )
 }
